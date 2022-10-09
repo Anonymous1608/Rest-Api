6d6f94afd925481ab1bae31914e76e567d2a8ec9
Aniket <aniketkalbande16@gmail.com>
2022-10-10 00:13:09 +0530

    Added add product endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const router = express.Router();
-import { registerController, loginController, userController, refreshController } from '../controllers';
+import { registerController, loginController, userController, refreshController, productController } from '../controllers';
 import auth from '../middlewares/auth';
 
 router.post('/register', registerController.register);
@@ -9,6 +9,7 @@ router.post('/login', loginController.login);
 router.get('/me', auth, userController.me);
 router.post('/refresh', refreshController.refresh);
 router.post('/logout',auth, loginController.logout);
+router.post('/products', productController.store);
 
 
 export default router;
